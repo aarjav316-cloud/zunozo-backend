343bff3e318336f49ac79616d350776221b87614
Aarjav jain <aarjav316@gmail.com>
2026-06-29 15:09:59 +0530

    validation global middleware

---
+++ src/middleware/validate.middleware.js
@@ -0,0 +1,27 @@
+const validate = (schema) => {
+  return (req, res, next) => {
+    try {
+
+      req.body = schema.parse(req.body);
+
+      next();
+
+    } catch (error) {
+
+      const formattedErrors = {};
+
+      for (const issue of error.issues) {
+        formattedErrors[issue.path[0]] = issue.message;
+      }
+
+      return res.status(400).json({
+        success: false,
+        message: "Validation Failed",
+        errors: formattedErrors,
+      });
+
+    }
+  };
+};
+
+export default validate;
