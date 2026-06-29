dd800cc7b3f4aac0dc28a3a1e6517c3b7ceabcf1
Aarjav jain <aarjav316@gmail.com>
2026-06-29 15:24:13 +0530

    generateSlug file

---
+++ src/modules/event/utils/generateSlug.js
@@ -0,0 +1,24 @@
+import Event from "../models/event.model";
+
+
+const generateSlug = async (title) => {
+  // Convert title into URL-friendly slug
+  const baseSlug = title
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "") // Remove special characters
+    .replace(/\s+/g, "-");    // Replace spaces with -
+
+  let slug = baseSlug;
+  let counter = 2;
+
+  // Check if slug already exists
+  while (await Event.findOne({ slug })) {
+    slug = `${baseSlug}-${counter}`;
+    counter++;
+  }
+
+  return slug;
+};
+
+export default generateSlug;
